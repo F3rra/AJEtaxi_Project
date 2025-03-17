83e13be81549a7ed844ffae529a082b83d22091c
F3rra <145546292+F3rra@users.noreply.github.com>
2025-03-17 13:47:02 +0100

    0.1

---
+++ website/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+
+    </body>
+    <footer>
+        <p>&copy; 2021 My Website</p>
+    </footer>
+</html>
